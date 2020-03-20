23370a2df33ee6980d49e8820597d28cf39e7cec
Hasanat Jahan <jahanhasanat@gmail.com>
2020-03-20 16:59:40 -0400

    The methods as they work

--- Lab 17.2/src/setAndMaps.java
+++ Lab 17.2/src/setAndMaps.java
@@ -4,23 +4,85 @@ public class setAndMaps {
 
    // Prints all movies that occur in both lists.
    public static String intersection(List<String> list1, List<String> list2) {
-      //Add code below
-      // TODO: HAVE NO IDEA YET
-      return "";
+      //this is adding all the items in list 1 to the hashset
+      Set<String> movies = new HashSet<>(list1);
+      Set<String> intersection = new HashSet<>(list2);
+      intersection.retainAll(movies);
+
+      //create an arraylist to hold matching values
+      ArrayList<String> matchingVals = new ArrayList<>();
+      for(String elem : intersection){
+         matchingVals.add(elem);
+      }
+      Collections.sort(matchingVals); //nlogn
+      String result = "";
+      for(String elem: matchingVals){
+         result = result + elem;
+         result = result + "\n";
+      }
+      return result;
    }
 
    // Prints all movies in the list that occur at least k times
    // (print the movie followed by the number of occurrences in parentheses).
-   public static String frequent(List<String> list, int k) {
-      //Add code below
-      return "";
+   public static String frequent(List<String> list, int k) { //The expected runtime must be O(nlogn) where n is the total number of movies in the list
+      // create a map to hold the values of movie name and count
+      Map<String, Integer> movieTMap = new TreeMap<String, Integer>();
+      for(String elem: list){
+         Integer num = movieTMap.get(elem);
+         if(num == null) movieTMap.put(elem, 1);
+         else movieTMap.put(elem, num + 1);
+      }
+      //using a for each loop with a map
+      String result = "";
+      for(Map.Entry<String, Integer> entry: movieTMap.entrySet()){
+         // if the values are the same then add to result
+         if(entry.getValue() == k){
+            result = result + entry.getKey() + " : " + entry.getValue();
+            result = result + "\n";
+         }
+      }
+      return result;
    }
 
    // Prints all movies in the list, grouped by number of characters.
    // All movies with the same number of characters are printed on the same line.
    // Movies with fewer characters are listed first.
+
+   //Source: https://www.quora.com/How-do-you-add-an-element-to-an-arraylist-thats-in-a-hashmap
    public static String groupByNumChars(List<String> list) {
-      //Add code below
-      return "";
+
+      //create a map to hold the count and the list together
+      Map<Integer, List<String>> movieMap = new TreeMap<Integer, List<String>>();
+      // for each elem in the list, count the num of characters
+      for(String elem : list){
+         int count = 0;
+         for(int i = 0; i< elem.length(); i++){
+            if(elem.charAt(i) != ' ') count++;
+         }
+
+         List<String> movieNames = movieMap.get(count);
+         // if list does not exist create it
+         if(movieNames == null){
+            movieNames = new ArrayList<String>();
+            movieNames.add(elem);
+            movieMap.put(count, movieNames);
+         }
+         // if there is a list
+         else {
+            if (!movieNames.contains(elem)) movieNames.add(elem);
+         }
+      }//for to populate the treemap
+
+      String result = "";
+      for(Map.Entry<Integer, List<String>> entry: movieMap.entrySet() ){
+         ArrayList <String> temp = (ArrayList) entry.getValue();
+         Collections.sort(temp);
+         for(String elem: temp){
+            result = result + elem + ",";
+         }
+         result = result + "\n";
+      }
+      return result;
    }
 }
